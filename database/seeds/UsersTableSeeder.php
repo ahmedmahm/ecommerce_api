518777880a0c9a6664fa0e261bfa766fbe80d167
Ahmed-Mahmoud <ahmedm.monte@gmail.com>
2020-07-20 19:49:47 +0200

    JWT Authentication

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's clear the users table first
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // Let's make sure everyone has the same password and
+        // let's hash it before the loop, or else our seeder
+        // will be too slow.
+        $password = Hash::make('12345678');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+
+    }
+}
