6f1a34881da641a62e0527db6494dc8e37d713b4
Ahmed-Mahmoud <ahmedm.monte@gmail.com>
2020-07-23 17:47:08 +0200

    make_me_api_get

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::group([
     Route::post('login', 'AuthController@login')->name('login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+    Route::get('me', 'AuthController@me');
 
 });
 
