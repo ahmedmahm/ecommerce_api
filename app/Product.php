a1daa96f572eed975143c41101845a6898bc97a2
Ahmed Mahmoud <ahmedmahmoud@LPT-A2141-11.local>
2020-07-30 17:01:47 +0200

    add_product_controller

--- app/Product.php
+++ app/Product.php
@@ -6,11 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = [
+        'id','name', 'qr' ,'price', 'description', 'image', 'category'
+    ];
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      * Each product has one order from one user
      */
     public function orders(){
         return $this->hasOne('App\Order');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
     }
 }
